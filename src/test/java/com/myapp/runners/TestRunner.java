54c4539af088f2a78a77a64dd1b004b6beaabb32
RitaFurman <margaret_furman@yahoo.com>
2020-12-20 22:20:17 -0600

    added report plugin to runner class

--- src/test/java/com/myapp/runners/TestRunner.java
+++ src/test/java/com/myapp/runners/TestRunner.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = { "json:target/cucumber.json"
+
+        },
         features = "src/test/resources/Features",
         glue = "com/myapp/step_definitions",
         dryRun = false
